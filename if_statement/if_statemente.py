bbce4ec909f5e4226647987103700ce68c685483
Nobuyoshi Ishizuka <nobuyoshi_japa@hotmail.com>
2020-10-23 11:00:16 -0300

    continue

---
+++ if_statement/if_statemente.py
@@ -0,0 +1,17 @@
+"""
+IF STATEMENT
+"""
+x = int(input("Plese enter a integer: "))
+
+if x < 0:
+    x = 0
+    print('Negative changed to zero.')
+
+elif x == 0:
+    print('Zero')
+
+elif x == 1:
+    print('Single')
+
+else:
+    print('More')
---
+++ loop_for/loop_for.py
@@ -0,0 +1,9 @@
+"""
+LOOP FOR
+
+Measure some Strings
+"""
+words = ['cat', 'window', 'defenestrate']
+
+for w in words:
+    print(w, len(w))
